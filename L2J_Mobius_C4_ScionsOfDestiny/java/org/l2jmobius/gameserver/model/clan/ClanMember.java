8c187e37d73dd42a654296d96ca2fe7a507fc331
Glockxvii <113671264+Glockxvii@users.noreply.github.com>
2026-02-26 13:36:29 +0100

    Clan fix nr2

--- L2J_Mobius_C4_ScionsOfDestiny/java/org/l2jmobius/gameserver/model/clan/ClanMember.java
+++ L2J_Mobius_C4_ScionsOfDestiny/java/org/l2jmobius/gameserver/model/clan/ClanMember.java
@@ -49,6 +49,7 @@ public class ClanMember
 	private int _raceOrdinal;
 	private Player _player;
 	private int _pledgeType;
+	private int _clanPrivileges;
 	
 	/**
 	 * Used to restore a clan member from the database.
@@ -73,6 +74,7 @@ public class ClanMember
 		_powerGrade = clanMember.getInt("power_grade");
 		_sex = clanMember.getInt("sex") != 0;
 		_raceOrdinal = clanMember.getInt("race");
+		_clanPrivileges = clanMember.getInt("clan_privs");
 	}
 	
 	/**
@@ -98,6 +100,7 @@ public class ClanMember
 		_title = player.getTitle();
 		_sex = player.getAppearance().isFemale();
 		_raceOrdinal = player.getRace().ordinal();
+		
 	}
 	
 	/**
@@ -118,10 +121,12 @@ public class ClanMember
 			_title = _player.getTitle();
 			_sex = _player.getAppearance().isFemale();
 			_raceOrdinal = _player.getRace().ordinal();
+			_clanPrivileges = _player.getClanPrivileges().getMask();
 		}
 		
 		if (player != null)
 		{
+			player.getClanPrivileges().setMask(_clanPrivileges);
 			if ((_clan.getLevel() > 3) && player.isClanLeader())
 			{
 				SiegeManager.getInstance().addSiegeSkills(player);
@@ -134,6 +139,12 @@ public class ClanMember
 		}
 		
 		_player = player;
+	}
+	
+	public int getClanPrivileges()
+	{
+		// Note the .getMask() at the end of _player.getClanPrivileges()
+		return _player != null ? _player.getClanPrivileges().getMask() : _clanPrivileges;
 	}
 	
 	/**
